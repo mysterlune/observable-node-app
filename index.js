b7f5b5bd005850905d8244d8f6b55b12cee85d80
River Lune <mysterlune@gmail.com>
2016-06-24 18:11:16 -0700

    Adding some dependency and docker-compose.yml action

--- index.js
+++ index.js
@@ -2,19 +2,54 @@ var restify = require('restify'),
   bunyan = require('bunyan'),
   util = require('util'),
   exec = require('child_process').exec,
-  fs = require('fs');
+  fs = require('fs'),
+  jsonApiFormatter = require('restify-formatter-jsonapi'),
+  _ = require('lodash'),
+  sprintf = require('sprintf-js').sprintf,
+  redis = require('redis');
 
-var server = restify.createServer({
-  name: 'helloworld'
+console.log('REDIS URL'+process.env.REDIS_URL);
+
+var redisClient = redis.createClient(process.env.REDIS_URL);
+
+redisClient.on('connect', function() {
+  console.log('connected to redis');
 });
 
-var port = process.env.EDGE_SERVICE_PORT || 8080;
+var GRAPH_SERVICE_HOST = process.env.GRAPH_SERVICE_HOST || '192.168.99.100',
+  EDGE_SERVICE_PORT = process.env.EDGE_SERVICE_PORT || 8080;
+
+// Ensure that the public dir exists...
+var publicDir = sprintf('%1s/public', __dirname);
+if (!fs.existsSync(publicDir)) {
+  fs.mkdirSync(publicDir);
+}
+
+// Ensure that the graphs dir exists...
+var graphsDir = sprintf('%1s/graphs', __dirname);
+if (!fs.existsSync(graphsDir)) {
+  fs.mkdirSync(graphsDir);
+}
+
+var server = restify.createServer({
+  name: 'helloworld',
+  // If more formatters are needed, just add to the merge...
+  'formatters': _.merge(jsonApiFormatter)
+});
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.authorizationParser());
 server.use(restify.dateParser());
 server.use(restify.queryParser());
 server.use(restify.urlEncodedBodyParser());
+
+server.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Resource', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
 
 server.use(function slowHandler(req, res, next) {
 
@@ -39,47 +74,90 @@ server.use(function slowHandler(req, res, next) {
 
 });
 
-server.get({path: '/hello/:name', name: 'GetFoo'}, function respond(req, res, next) {
-  res.send({
-    hello: req.params.name
-  });
-  return next();
-});
+server.get({path: '/record', name: 'Record'}, function recordRespond(req, res, next) {
 
-server.get({path: '/process', name: 'ProcessDump'}, function respond(req, res, next) {
-  console.log(process);
-  res.send('logged the process!');
-  return next();
-});
+  fs.appendFileSync(sprintf('/tmp/perf-%1s.map', process.pid));
 
-server.get({path: '/record', name: 'Record'}, function respond(req, res, next) {
-
-  // should use sprintf...
-  fs.appendFileSync('/tmp/perf-'+process.pid+'.map');
-
-  exec('/src/record.sh', function record(error, stderr, stdout) {
+  exec(sprintf('%1s/record.sh', __dirname), function recordExecute(error, stderr, stdout) {
     console.log(error);
   });
 
-  res.send({ hey: 'recorded' });
+  res.setHeader('content-type', 'application/json');
+  res.send({});
 });
 
-server.get({path: '/graph', name: 'Graph'}, restify.serveStatic({
-  directory: __dirname,
-  file: 'out.nodestacks01.svg'
+server.get({path: '/recordings', name: 'Recordings'}, function getRecordingsResponder(req, res, next) {
+
+  fs.readdir(publicDir, function(err, files) {
+
+    var data = _.map(files, function(a, b, c) {
+      return {
+        "type": "recording",
+        "id": a,
+        "attributes": {
+          "graph-name": a,
+          "graph-source": sprintf('http://%1s/public/%2s', GRAPH_SERVICE_HOST, a)
+        }
+      };
+    });
+
+    res.send(data);
+  });
+});
+
+server.get({path: '/graphs/:name', name: 'GraphLookup'}, function getGraphResponder(req, res, next) {
+
+  fs.readdir(publicDir, function(err, files) {
+
+    var data = _.filter(files, req.params.name, function(a, b, c) {
+      return {
+        "type": "recording",
+        "id": a,
+        "attributes": {
+          "graph-name": a,
+          "graph-source": sprintf('http://%1s/public/%2s', GRAPH_SERVICE_HOST, a)
+        }
+      };
+    });
+
+    res.send(data);
+
+  });
+
+});
+
+// Serves the static assets produced by ./record.sh
+server.get(/\/public\/?.*/, restify.serveStatic({
+  directory: __dirname
 }));
 
-server.get({path: '/stack', name: 'Stack'}, restify.serveStatic({
-  directory: __dirname,
-  file: 'out.nodestacks01'
-}));
+// server.get({path: '/stack', name: 'Stack'}, restify.serveStatic({
+//   directory: __dirname,
+//   file: 'out.nodestacks01'
+// }));
 
-server.get({path: '/map', name: 'Map'}, restify.serveStatic({
-  directory: '/tmp',
-  file: 'perf-'+process.pid+'.map'
-}));
+// server.get({path: '/map', name: 'Map'}, restify.serveStatic({
+//   directory: '/tmp',
+//   file: 'perf-'+process.pid+'.map'
+// }));
 
+// server.get({path: '/process', name: 'ProcessDump'}, function processDumpRespond(req, res, next) {
+//   console.log(process);
+//   res.send('logged the process!');
+//   return next();
+// });
 
-server.listen(port, function() {
+server.get({path: '/hammer', name: 'Hammer'}, function getHammerRespond(req, res, next) {
+
+  redisClient.incr('hits', function(err, reply) {
+    console.log(reply);
+    res.send({
+      hammer: 'bammer'
+    });
+  });
+
+});
+
+server.listen(EDGE_SERVICE_PORT, function() {
   console.log('listening: %s', server.url);
 });
--- record.sh
+++ record.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 perf record -F 99 -p `pgrep -n node` -g -- sleep 30
-perf script > /src/out.nodestacks01
+
+SOURCENAME="out.nodestacks$RANDOM"
+SOURCEPATH="/src/$SOURCENAME"
+
+perf script > $SOURCEPATH
 cd /src/FlameGraph
-./stackcollapse-perf.pl < ../out.nodestacks01 | ./flamegraph.pl --colors js > ../out.nodestacks01.svg
+./stackcollapse-perf.pl < $SOURCEPATH | ./flamegraph.pl --colors js > "../public/$SOURCENAME.svg"
